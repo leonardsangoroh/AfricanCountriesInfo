4c60c20f7cdf7efce42402c879e23676c85daa59
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-14 15:04:35 +0300

    feat: add JSON import architecture

---
+++ AfricanCountriesInfo/Countries.swift
@@ -0,0 +1,8 @@
+//
+//  Countries.swift
+//  AfricanCountriesInfo
+//
+//  Created by Lee Sangoroh on 14/02/2024.
+//
+
+import Foundation
---
+++ AfricanCountriesInfo/Country.swift
@@ -0,0 +1,8 @@
+//
+//  Country.swift
+//  AfricanCountriesInfo
+//
+//  Created by Lee Sangoroh on 14/02/2024.
+//
+
+import Foundation
